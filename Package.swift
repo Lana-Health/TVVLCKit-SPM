51a155f812c47719a9cb7b31db4ce197cb08d709
apoorva-lana <97281393+apoorva-lana@users.noreply.github.com>
2026-02-05 16:57:26 +0530

    Update Package.swift
    Signed-off-by: apoorva-lana <97281393+apoorva-lana@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TVVLCKitTarget",
-            url: "https://github.com/Lana-Health/TVVLCKit-SPM/releases/download/v3.6.0/TVVLCKit.xcframework.zip",
+            url: "https://github.com/Lana-Health/TVVLCKit-SPM/releases/v3.6.0/TVVLCKit.xcframework.zip",
             checksum: "f232cab7fd30d3bf05f0b8ef1e917753d7f0d6d54ea25adf39d4eb1b149d4d49"
         )
     ]
