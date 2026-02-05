3f924c9c3e901cbd150be0091a663f6c59f05572
apoorva <apoorva.chandrakar@lana.health>
2026-02-05 16:29:16 +0530

    chore: point manifest to remote binary

---
+++ Package.swift
@@ -0,0 +1,23 @@
+
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TVVLCKit-SPM",
+    platforms: [
+        .tvOS(.v13) // Set this to your minimum requirement
+    ],
+    products: [
+        .library(
+            name: "TVVLCKit",
+            targets: ["TVVLCKitTarget"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "TVVLCKitTarget",
+            url: "https://github.com/Lana-Health/TVVLCKit-SPM/releases/download/v3.5.0/TVVLCKit.xcframework.zip",
+            checksum: "f232cab7fd30d3bf05f0b8ef1e917753d7f0d6d54ea25adf39d4eb1b149d4d49"
+        )
+    ]
+)
